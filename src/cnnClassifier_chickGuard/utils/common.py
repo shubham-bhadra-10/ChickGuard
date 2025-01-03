9d0e2b6d48ad38f62899bc50d3b7cf090ccc3b20
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-01-03 16:08:22 -0500

    Function to save and load bin file in common.py

--- src/cnnClassifier_chickGuard/utils/common.py
+++ src/cnnClassifier_chickGuard/utils/common.py
@@ -75,3 +75,30 @@ def load_json(path: Path) -> ConfigBox:
 
     logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
